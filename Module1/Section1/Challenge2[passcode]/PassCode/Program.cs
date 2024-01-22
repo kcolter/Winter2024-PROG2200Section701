e022517c6b193cee473b7a3421257083d937f496
KeeganLaptop <kcolter3@gmail.com>
2024-01-22 08:53:16 -0400

    rewrote code and added pw-change feature as per instruction doc

--- Module1/Section1/Challenge2[passcode]/PassCode/Program.cs
+++ Module1/Section1/Challenge2[passcode]/PassCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PassCode
 {
@@ -6,16 +7,28 @@ namespace PassCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the pass code?");
-            var code = Console.ReadLine();
+            var password = "secret";
+            var code = ""; //initialize code to empty string
 
-            if(code == "secret")
+            while(code != password)
             {
-                Console.WriteLine("Authenticated");
-            }
-            else if(code != "secret")
-            {
-                Console.WriteLine("Not Authenticated");
+                Console.WriteLine("What is the pass code?");
+                code = Console.ReadLine();
+
+                if(code == password)
+                {
+                    Console.WriteLine("Authenticated.\n Would you like to update your passcode? [y/n]");
+                    var response = Console.ReadLine();
+
+                    if (response.ToLower() == "y")
+                    {
+                        Console.WriteLine("Please enter the new passcode. You'll be asked to re-verify after changing.");
+                        password = Console.ReadLine(); //update password. this change will also cause our while to run again
+                        continue; //restart loop
+                    }
+                    break;
+                }
+                Console.WriteLine("Not Authenticated.");
             }
         }
     }
