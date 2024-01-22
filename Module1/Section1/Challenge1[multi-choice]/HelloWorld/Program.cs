f5afa44bda12542a0763d1bf7381a3ad1eca40f7
KeeganLaptop <kcolter3@gmail.com>
2024-01-22 08:28:12 -0400

    added 2 questions to Challenge 1 Mod1, as per Lab inst.

--- Module1/Section1/Challenge1[multi-choice]/HelloWorld/Program.cs
+++ Module1/Section1/Challenge1[multi-choice]/HelloWorld/Program.cs
@@ -15,9 +15,18 @@ namespace HelloWorld
             Console.WriteLine("What month were you born in?");
             var month = Console.ReadLine();
 
+            Console.WriteLine("What is your quest?");
+            var quest = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite colour?");
+            var colour = Console.ReadLine();
+
+
             Console.WriteLine("Your name is {0}", name);
             Console.WriteLine("Your age is {0}", age);
             Console.WriteLine("Your birth month is {0}", month);
+            Console.WriteLine("Your quest is {0}", quest);
+            Console.WriteLine("Your favorite colour is {0}", colour);
         }
     }
 }
